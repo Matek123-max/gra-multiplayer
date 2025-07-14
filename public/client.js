53c8db73f2d6ad9d78313e5d23ecc5584c4fa53e
Matek123-max <mfraczek08@outlook.com>
2025-07-14 17:00:57 +0200

    Create client.js

---
+++ public/client.js
@@ -0,0 +1,24 @@
+const socket = io();
+
+function joinRoom() {
+  const username = document.getElementById("username").value;
+  const room = document.getElementById("room").value;
+  if (!username || !room) return alert("Uzupełnij pola!");
+
+  socket.emit("joinRoom", { room, username });
+}
+
+socket.on("roomList", (rooms) => {
+  const list = document.getElementById("roomList");
+  list.innerHTML = "";
+  rooms.forEach((r) => {
+    const li = document.createElement("li");
+    li.textContent = `${r.name} (${r.count}/5)`;
+    list.appendChild(li);
+  });
+});
+
+socket.on("gameStart", ({ word }) => {
+  document.getElementById("game").style.display = "block";
+  document.getElementById("word").textContent = word;
+});
